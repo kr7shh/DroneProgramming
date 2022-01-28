5dc3c94327604cc27cb1a642e358847ee4a21a35
kr7shh <68024061+kr7shh@users.noreply.github.com>
2022-01-28 00:15:27 -0500

    uploading drone files
    Uploading files

---
+++ ImageCapturing.py
@@ -0,0 +1,15 @@
+import cv2
+from djitellopy import tello
+from time import sleep
+
+drone = tello.Tello()
+drone.connect()
+print(drone.get_battery())
+
+drone.streamon()
+while True:
+    img = drone.get_frame_read().frame
+    img = cv2.resize(img,(360,240))
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
+
